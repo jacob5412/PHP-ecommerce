c58aa0656016c5227750a3ab1a5d7e23bc08ddde
Jacob John <kapsjacob@gmail.com>
2019-11-15 08:00:29 +0530

    Update secondfooter.php

--- includes/secondfooter.php
+++ includes/secondfooter.php
@@ -4,9 +4,9 @@
             <div class="col s12 l4 ">
                 <h5 class="grey-text text-lighten-3 animated slideInUp wow">Developers</h5>
                 <ul>
-                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="https://github.com/jacobjohn2016"><i class="tiny material-icons" style="position:relative;top:3px">developer_mode</i> Jacob John (16BCE2205)</a></li>
-                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href=""><i class="tiny material-icons" style="position:relative;top:3px">local_library</i> K Venkata Abhishek (16BCI0168)</a></li>
-                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href=""><i class="tiny material-icons" style="position:relative;top:3px">message</i> G V Vivek (18BIT0469)</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="https://github.com/jacobjohn2016"><i class="tiny material-icons" style="position:relative;top:3px">developer_mode</i> Jacob John</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="https://smakosh.com/"><i class="tiny material-icons" style="position:relative;top:3px">developer_mode</i>Ismail Ghallou</a></li>
+                    <li></li>
                 </ul>
             </div>
             <div class="col s12 l4 ">
@@ -31,7 +31,7 @@
     <div class="footer-copyright">
         <div class="container">
             © <?= Date('Y'); ?> All rights reserved
-            <a class="grey-text text-lighten-4 right animated slideInUp wow" href="https://smakosh.com/"><i class="fa fa-heartbeat animated pulse infinite red-text"></i> Reproduced for Educational Purposes. Original work by Ismail Ghallou.</a>
+            <a class="grey-text text-lighten-4 right animated slideInUp wow" href="https://smakosh.com/"><i class="fa fa-heartbeat animated pulse infinite red-text"></i>Ismail Ghallou and Jacob John</a>
         </div>
     </div>
 </footer>
